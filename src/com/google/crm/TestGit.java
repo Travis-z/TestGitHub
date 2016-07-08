4246d077496d4a57a5f724f7b2c766ded656445f
Travis-z <380726248@qq.com>
2016-07-09 01:34:18 +0800

    update 02

--- src/com/google/crm/TestGit.java
+++ src/com/google/crm/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub ....");
 		System.out.println("update 01");
+		System.out.println("update 02");
 	}
 }
